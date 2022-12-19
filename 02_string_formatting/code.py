cbe455b5803ac7e66f5b68b82daf1c4e89f04336
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-12-20 01:40:32 +0400

    added

--- 02_string_formatting/code.py
+++ 02_string_formatting/code.py
@@ -1,7 +1,26 @@
 name = "Bob" 
 
-print(f"Hello, {name}") #! f - позволяет менять строку внутри и вставлять значения какие угодно в момент времени получения
+#print(f"Hello, {name}") #! f - позволяет менять строку внутри и вставлять значения какие угодно в момент времени получения
 
 name = "Rolf"
 
-print(f"Hello, {name}")
+#print(f"Hello, {name}")
+
+
+#!------Strings with fomat method
+
+name = "Pidaraz"
+greeting = "Hello, {}"
+with_name = greeting.format(name) #! format - заменяет скобки на значения которые в него заложены
+with_name_two = greeting.format("ebanat");
+
+#print(with_name)
+#print(with_name_two)
+
+#!------Loger tempplates
+
+longer__phrase = "Hello , {}. Today is {}."
+
+formatted = longer__phrase.format("Eblan", "Monday")
+
+print(formatted)

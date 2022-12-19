387d54aa9370eed9e82b593ffbf055a1e731c418
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-12-20 01:30:29 +0400

    added

--- 01_variables/code.py
+++ 01_variables/code.py
@@ -1,36 +1,17 @@
-# -- Numbers and Floats --
-
-x = 15
+x = 15 #---точка с запетой не нужна
 price = 9.99
+discount=0.22
+result = price * (1-discount);
+#print(result) #----печать значения
 
-discount = 0.2
+#!-----STRINGS
 
-result = price * (1 - discount)
+name = "Pidaraz"
+name = "Suka"
+#print(name)
 
-print(result)
+#!-----CHANGE VALUES --- строки и значения переменных меняются поочередно последовательно сверху вниз
 
-# -- Strings --
-
-name = "Rolf"
-name = "Rolf"
-
-print(name)
-print(name * 2)
-
-# -- Changing variables --
-# Variables are names for values.
-
-a = 25
-b = a
-
-# Here we've given the value '25' the names 'a' and 'b'.
-
-print(a)
-print(b)
-
-b = 17
-
-# Here we've given the value '17' the name 'b'. The name 'a' is still a name for '25'!
-
-print(a)
+a =25 
+b = a  
 print(b)

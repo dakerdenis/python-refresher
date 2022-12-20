c8804511adc2ecd34e27dfea02901310272aee42
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-12-21 02:15:59 +0400

    фввув

--- 06_advanced_set_operations/code.py
+++ 06_advanced_set_operations/code.py
@@ -30,5 +30,5 @@ science = {"Bob", "Jen", "Adam", "Anne"}
 
 # Given these two sets of students, we can calculate those who do both art and science by using `.intersection`
 
-both = art.intersection(science)
-print(both) #!---
+both = art.intersection(science) #!--- выдаёт значение в которых есть пересечение со значениями 
+print(both) 

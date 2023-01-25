d5934a6efa80191fb085a56712fd03d1628b5602
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-01-25 14:06:53 +0400

    addded

---
+++ learn.py
@@ -0,0 +1,50 @@
+class Test__learning:
+    def print_name(self,name):
+        print("message1:{}  message2: {}".format(name , name + ' changed'))
+
+    def say_hello(self):
+        self.print_name("stalker")
+
+
+test1 = Test__learning()
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print(f"Name: {self.name}   Age: {self.age}")
+
+
+tom  = Person("Tom",23)
+bob = Person("Bob", 38)
+
+tom.display_info()
+bob.display_info()
+
+
+class Count:
+    def __init__(self,number1,number2):
+        self.number1 = number1
+        self.number2 = number2
+
+
+    def display_info(self):
+        number =  self.number1 + self.number2
+        return number
+
+num = Count(1,2)
+print(num.display_info())

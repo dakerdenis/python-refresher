44a3476578a676ebf1fbad4d81efb9623f63ce72
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-01-26 13:15:47 +0400

    фввув

--- learn.py
+++ learn.py
@@ -36,34 +36,59 @@ bob = Person("Bob", 38)
 #bob.display_info()
 
 
-class Count:
-    def __init__(self,number1,number2):
-        self.number1 = number1
-        self.number2 = number2
-
-
-    def display_info(self):
-        number =  self.number1 + self.number2
-        return number
-
-num = Count(1,2)
-#print(num.display_info())
-
-
-class Dog:
-
-    attr1 = "mammal"
-
+class Store:
     def __init__(self,name):
         self.name = name
+        self.items = []
 
-    def speak(self):
-        print("My name is {}".format(self.name))
+    def add_item(self,name,price):
+        self.items.append({
+            'name': name,
+            'price': price
+        })
+
+    def stock__price(self):
+        total = 0
+        for item in self.items:
+            total +=item['price']
+        return total
+    
+    @classmethod
+    def franchise(cls, store):
+        return cls(store.name + " - franchise")
+    
+    @staticmethod
+    def store_details(store):
+        return '{} , total stock price: {}'.format(store.name, int(store.stock_price()))
+
+    @staticmethod
+    def all_items_and_name(store):
+        print("Shop: {}".format(store.name))
+        i=1
+        list1 = store.items
+        for n in list1:
+
+            print("{}) {} , with price {} $".format(i, n['name'] , n['price']))
+            i+=1
+        print("Total amount of orders: {}".format(store.stock__price()))
 
 
-Rodger = Dog("Rodger")
-Tommy = Dog("Tommy")
+#создаём обьекты
+store = Store("Test")
+store2 = Store("Amazon")
+
+#добавляем 3 элемента в магазин
+
+store2.add_item("Keyboard", 100)
+store2.add_item("Rx 6600xt", 160)
+store2.add_item("Book", 130)
+store2.add_item("Table", 420)
+store2.add_item("Stalker", 50)
 
 
-Rodger.speak()
-Tommy.speak()
+
+
+
+
+#выдаёт количество товаров в магазине
+Store.all_items_and_name(store2)

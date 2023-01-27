0f8d927e8b9755183c73380ed4f2ac20effca7a9
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-01-27 17:02:46 +0400

    added

--- 32_errors_in_python/code.py
+++ 32_errors_in_python/code.py
@@ -1,15 +1,33 @@
 def divide(dividend, divisor):
     if divisor == 0:
-        raise ZeroDivisionError("Division")    #эта ошибка идёт вместе с питоном
+        raise ZeroDivisionError("Division can not be 0")    #эта ошибка идёт вместе с питоном
+
 
     return dividend / divisor
 
 
-grades = []
+students = [
+    {"name": "Bob", "grades":[75,13,14]},
+    {"name": "Alex", "grades":[0]},
+    {"name": "Tom", "grades":[12,14]}
+]
 
 print("Welcome to the average grade program.")
-if len(grades) == 0:
-    print("You don't have grades")    
-average = divide(sum(grades),len(grades))
 
-print(f"Average marks: {average}")
+try:
+    for student in students:
+        name = student['name']
+        grades = student['grades']        
+        average = divide(sum(grades),len(grades))    
+        print(f"{name}  average grade is {average}")
+        
+except ZeroDivisionError as e:   #it creates - variable e and put there error message
+    print("there are no grades yet in your list.")    
+else: 
+    print(f"The average grade is {average}")
+finally:
+    print("Thank you!")
+
+    
+
+ 

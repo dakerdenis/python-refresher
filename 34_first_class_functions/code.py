ba6b053895ed800f4b7f0df188c05c1de0200a59
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-01-30 14:53:06 +0400

    added

--- 34_first_class_functions/code.py
+++ 34_first_class_functions/code.py
@@ -6,6 +6,37 @@ def devide(dividend, divisor):
 def calculate (*values, operator):       # *values - collect values and put them into toples
     return operator(*values)
 
-result = calculate(2,4 , operator=devide)
+result = calculate(2,4 , operator=devide)   # function - here could be variable 
+
+
+
+
+
+
+
+
+
+
+
+
+def search(sequence, expected,finder):
+    for elem in sequence:
+        if finder(elem) == expected:
+            return elem
+    raise RuntimeError(f"Could not find an element with {expected}.")
+
+friends = [
+    {"name": "Adolf Smith", "age": 24},
+    {"name": "Dolf Lungden", "age": 35},
+    {"name": "Arnold Schwarzeneger", "age": 29}
+]
+
+
+def get_frined_name(friend):
+    return friend["name"]
+
+expected_value = "Bob Smith"
+
+result = search(friends,expected_value,get_frined_name)
 
 print(result)

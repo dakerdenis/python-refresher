f94efcaa66ac070697087f294bdeb7390babad52
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-02-02 13:44:04 +0400

    added

--- 36_the_at_syntax_for_decorators/code.py
+++ 36_the_at_syntax_for_decorators/code.py
@@ -1,39 +1,34 @@
-user = {"username": "jose", "access_level": "guest"}
-
-
-def make_secure(func):
-    def secure_function():
-        if user["access_level"] == "admin":
-            return func()
-        else:
-            return f"No admin permissions for {user['username']}."
-
-    return secure_function
-
-
-@make_secure
-def get_admin_password():
-    return "1234"
-
-
-# -- keeping function name and docstring --
 import functools
+    
+user = {"username": "jose", "access_level": "admin"}
 
 
-user = {"username": "jose", "access_level": "guest"}
 
 
 def make_secure(func):
     @functools.wraps(func)
     def secure_function():
-        if user["access_level"] == "admin":
+        if user["access_level"]  =="admin":
             return func()
         else:
-            return f"No admin permissions for {user['username']}."
-
+            return f"No admin permission for {user['username']}."
+    
     return secure_function
 
 
-@make_secure
-def get_admin_password():
-    return "1234"
+@make_secure   #get_admin_password = make_secure(get_admin_password)
+def get_password(panel):
+    if panel == "admin":
+        return "1234"
+    elif panel == "billing":
+        return "super_secure_password"
+    
+
+
+
+print(get_password("billing"))
+
+
+
+
+
